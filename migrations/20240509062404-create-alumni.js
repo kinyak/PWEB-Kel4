a38f20859865b76b333f748ccdeffaa52c72c07f
ppaizzz <145271637+ppaizzz@users.noreply.github.com>
2024-05-12 22:28:22 +0800

    update

---
+++ migrations/20240509062404-create-alumni.js
@@ -0,0 +1,65 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('alumni', {
+      nim: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      date_of_birth: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      departemen: {
+        type: Sequelize.STRING
+      },
+      pekerjaan: {
+        type: Sequelize.STRING
+      },
+      perusahaan: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      jabatan: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      
+      role: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('alumni');
+  }
+};

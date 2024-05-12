3c803768bd25269c917c63efc9d7efc052c4072d
meeutia <145307246+meeutia@users.noreply.github.com>
2024-05-12 22:03:21 +0800

    upload fungsional autentikasi

---
+++ middleware/islogin.middleware.js
@@ -0,0 +1,32 @@
+const jwt = require ('jsonwebtoken')
+
+function isLogin(req, res, next) {
+  const token = req.cookies.token;
+
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET_TOKEN, function (err, decoded) {
+      if (err) {
+        return res
+          .status(500)
+          .send({
+            auth: false,
+            message: "Gagal untuk melakukan verifikasi token.",
+          });
+      }
+
+      req.userId = decoded.id;
+      req.userRole = decoded.role;
+      req.userEmail = decoded.email;
+    });
+    if (req.userRole == "alumni") {
+      return res.redirect("/home");
+    } else if (req.userRole == "admin") {
+      return res.redirect("/admin/dashboard");
+    }
+  }
+
+  next();
+}
+
+module.exports = isLogin;

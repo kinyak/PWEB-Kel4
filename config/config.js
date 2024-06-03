0bf9d58f79f6b2dccd0c28c962449eb98dfa5837
ppaizzz <145271637+ppaizzz@users.noreply.github.com>
2024-06-03 20:28:59 +0800

    fix config sequelize

--- config/config.js
+++ config/config.js
@@ -1,24 +1,28 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "alumnifti",
-    "host": "localhost",
-    "port" : 3307,
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'alumnifti',
+    host: 'localhost',
+    port: 3307,
+    dialect: 'mysql',
+    define: {
+      freezeTableName: true
+    },
+    logging: console.log // Enable logging to console
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   }
-}
+};
